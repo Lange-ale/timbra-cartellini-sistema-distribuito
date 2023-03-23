f88e16bb1a95a87122c98ea33a4df5bb550308c5
_.pamela___ <72273302+pamelaloda@users.noreply.github.com>
2023-03-23 11:39:08 +0100

    UI Svelte initialized

---
+++ UISvelte/src/App.svelte
@@ -0,0 +1,58 @@
+<script>
+import {login} from './store.js';  
+ 
+//function login(){}
+</script>
+
+<main style="background-color: blue;">
+  <h1>TIMBRA CARTELLINI</h1>
+
+  <p>
+    Inserisci il numero di matricola
+  </p>
+
+  <input type="text" placeholder="Matricola"/>
+
+  <button on:click={login()}>
+    LogIn
+  </button>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
