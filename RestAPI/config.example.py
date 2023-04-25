8e7d98ae14c158f99429c42f892e943e5dbaf8b7
Langellotti Alessio <alessiolangellotti2005@gmail.com>
2023-04-25 14:38:45 +0200

    init db and firebase accounts

---
+++ RestAPI/config.example.py
@@ -0,0 +1,31 @@
+firebase_config = {
+    "apiKey": "here your api key",
+    "authDomain": "here your auth domain",
+    "databaseURL": "", #required by the library but not used
+    "projectId": "here your project id", 
+    "storageBucket": "here your storage bucket",
+    "messagingSenderId": "here your messaging sender id",
+    "appId": "here your app id",
+    "measurementId": "here your measurement id"
+}
+
+db_config = {
+    "host": "localhost",
+    "database": "example",
+    "user": "example",
+    "password": "example",
+    "port": 54320
+}
+
+accounts = {
+    "example@domain.com": {
+        "name": "example",
+        "surname": "example",
+        "password": "example"
+    },
+    "john@doe.com": {
+        "name": "john",
+        "surname": "doe",
+        "password": "johndoe"
+    }   
+}
