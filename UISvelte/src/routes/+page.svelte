0fc4d56493b2e3221ce7aef79996b8f5016bc679
Giacomo Garoffoli <94188342+garoffoli@users.noreply.github.com>
2023-04-26 16:37:01 +0200

    aggiunta funzione nascondi

--- UISvelte/src/routes/+page.svelte
+++ UISvelte/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 import {matricola} from './store.js';  
- 
+
+let visibile = false;
+
 function get_worker_data(){
   fetch('http://localhost:5000/workers/' + $matricola)
     .then((response) => response.json())
@@ -8,7 +10,9 @@ function get_worker_data(){
     console.log(json);
   });
 }
-
+function toggle(){
+ visibile = !visibile
+}
 </script>
 
 <main class="min-h-screen" style="background-color: pink;">
